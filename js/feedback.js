d6cc3a978a255ef1cf644f04a425e0446987dd3b
basis64computer <basis64computer@gmail.com>
2024-05-27 08:37:51 +0800

    Feedback update

--- js/feedback.js
+++ js/feedback.js
@@ -11,7 +11,14 @@ form.addEventListener("submit", e => {
 	let name = document.querySelector("#name");
 	let message = document.querySelector("#message");
 
-	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.ChatID}&parse_mode=markdown&text=*Pesan dari ${name.value}*%0A${message.value}`, {
+	let name_value = name.value;
+	let message_value = message.value;
+
+
+	document.getElementById("name").value = "";
+	document.getElementById("message").value = "";
+
+	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.ChatID}&parse_mode=markdown&text=*Pesan dari ${name_value}*%0A${message_value}`, {
 		method: "GET"
 	}).then(success => {
 		alert("Masukkan Anda berhasil dikirim.");
