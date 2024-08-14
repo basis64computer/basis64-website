bdbec33a0114ee3648a5973cbc21e26b2ef16990
basis64computer <basis64computer@gmail.com>
2024-08-14 08:54:54 +0800

    Update feedback.js

--- js/feedback.js
+++ js/feedback.js
@@ -1,32 +1,7 @@
-let feedbackForm = document.querySelector("#feedbackForm");
-
 let bot = {
 	TOKEN: "6782845267:AAGBdmLNBYXJL5D3mmjvCq2qrY_tlysSY5Y",
 	ChatID: "5981475588",
 }
-
-feedbackForm.addEventListener("submit", e => {
-	e.preventDefault();
-
-	let name = document.querySelector("#name");
-	let message = document.querySelector("#message");
-
-	let name_value = name.value;
-	let message_value = message.value;
-
-
-	document.getElementById("name").value = "";
-	document.getElementById("message").value = "";
-
-	fetch(`https://api.telegram.org/bot${bot.TOKEN}/sendMessage?chat_id=${bot.ChatID}&parse_mode=markdown&text=*Pesan dari ${name_value}*%0A${message_value}`, {
-		method: "GET"
-	}).then(success => {
-		alert("Masukkan Anda berhasil dikirim.");
-	}, error => {
-		alert("Masukkan Anda tidak dapat dikirim.")
-		console.log(error);
-	})
-})
 
 function send_message(message) {
 	let msg = message.replace("_", "\\_").replace("_bold_", "*").replace("_italic_", "_")
